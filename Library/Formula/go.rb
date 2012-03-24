ab4042f902b632e2dc8608eac537a6f0f7251336
Brian P O'Rourke <bpo@somnambulance.net>
2012-03-23 17:16:24 -0700

    Go weekly build.
    * dropped outdated git repo
    * dropped old release r60.3
    * added optional test run
    
    I'll update this formula to make weekly the 'devel' build when the next
    release hits.

--- Library/Formula/go.rb
+++ Library/Formula/go.rb
@@ -2,35 +2,30 @@ require 'formula'
 
 class Go < Formula
   homepage 'http://golang.org'
-  version 'r60.3'
+  version 'weekly'
 
-  if ARGV.include? "--use-git"
-    url 'https://github.com/tav/go.git', :tag => 'release.r60.3'
-    head 'https://github.com/tav/go.git'
-  else
-    url 'http://go.googlecode.com/hg/', :revision => 'release.r60.3'
-    head 'http://go.googlecode.com/hg/'
-  end
+  url 'http://go.googlecode.com/hg/', :revision => 'weekly'
+  head 'http://go.googlecode.com/hg/'
 
   skip_clean 'bin'
 
   def options
-    [["--use-git", "Use git mirror instead of official hg repository"]]
+     [["--run-tests", "Test the new build before installing"]]
   end
 
   def install
-    prefix.install %w[src include test doc misc lib favicon.ico AUTHORS]
+    prefix.install %w[src include test doc api misc lib favicon.ico AUTHORS]
     cd prefix do
       mkdir %w[pkg bin]
 
-      # The version check is due to:
-      # http://codereview.appspot.com/5654068
-      version = ARGV.build_head? ? 'default' : 'release.r60.3 9516'
       File.open('VERSION', 'w') {|f| f.write(version) }
 
-      # Tests take a very long time to run. Build only
       cd 'src' do
-        system "./make.bash"
+        if ARGV.include? "--run-tests"
+          system "./all.bash"
+        else
+          system "./make.bash"
+        end
       end
 
       # Don't need the src folder, but do keep the Makefiles as Go projects use these
